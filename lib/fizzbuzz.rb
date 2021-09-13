96f49d9c20b62ebf4580451e392556ab1a61b4ce
zpeng1997 <zpeng1997@gmail.com>
2021-09-13 19:11:20 +0800

    888-case

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,7 +3,7 @@ class Fizzbuzz
     if number.to_s.include? '7'
       'GitHub7777777777777'
     elsif number % 3 == 0 && number % 5 == 0
-      'fizzbuzz'
+      'fizzbuzz888888888888'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0

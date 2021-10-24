a70fedb59d3d6c772a795ea4cd32123d72cd3506
Algo-Goer <xuebibaobei@163.com>
2021-10-25 02:11:20 +0800

    Add output GitHub

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+class Fizzbuzz
+    def calculate number 
+        if number % 3 == 0 && number % 5 == 0
+            'fizzbuzz'
+        elsif number % 3 == 0
+            'fizz'  
+        elsif number % 5 == 0    
+            'buzz'
+        else
+            number 
+        end
+    end 
+end   

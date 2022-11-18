28e76f26f15ff726539e1fa17a1a447b83a65434
jiahui.huang <jiahui.huang@juphoon.com>
2022-11-18 15:21:05 +0800

    Add output Github

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,7 +5,7 @@ class Fizzbuzz
     elsif number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
     elsif number % 3 == 0
-      'fizz'
+      'fizz1'
     elsif number % 5 == 0
       'buzz'
     else
